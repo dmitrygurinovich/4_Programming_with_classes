38ce4f23843c682f0d473637ec2f1bb6f53d25b0
Dmitry Gurinovich <dmitry.gurinovich@hotmail.com>
2021-03-18 19:42:50 +0300

    Task completed

--- Task12/src/main/java/by/epam/classes/entity/Car.java
+++ Task12/src/main/java/by/epam/classes/entity/Car.java
@@ -1,4 +1,153 @@
 package by.epam.classes.entity;
 
 public class Car {
+    private String manufacturer;
+    private String model;
+    private double maxVolumeOfTank;
+    private double volumeOfFuel;
+    private int maxSpeed;
+    private int speed;
+    private Engine engine;
+    private Wheel wheel;
+
+    public Car(String manufacturer, String model, int maxVolumeOfTank, int maxSpeed, Engine engine, Wheel wheel) {
+        this.manufacturer = manufacturer;
+        this.model = model;
+        this.maxVolumeOfTank = maxVolumeOfTank;
+        this.volumeOfFuel = 0;
+        this.maxSpeed = maxSpeed;
+        this.speed = 0;
+        this.engine = engine;
+        this.wheel = wheel;
+    }
+
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    public void setManufacturer(String manufacturer) {
+        this.manufacturer = manufacturer;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public double getMaxVolumeOfTank() {
+        return maxVolumeOfTank;
+    }
+
+    public void setMaxVolumeOfTank(int maxVolumeOfTank) {
+        this.maxVolumeOfTank = maxVolumeOfTank;
+    }
+
+    public double getVolumeOfFuel() {
+        return volumeOfFuel;
+    }
+
+    public void setVolumeOfFuel(double d) {
+        this.volumeOfFuel = d;
+    }
+
+    public int getMaxSpeed() {
+        return maxSpeed;
+    }
+
+    public void setMaxSpeed(int maxSpeed) {
+        this.maxSpeed = maxSpeed;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+
+    public Engine getEngine() {
+        return engine;
+    }
+
+    public void setEngine(Engine engine) {
+        this.engine = engine;
+    }
+
+    public Wheel getWheel() {
+        return wheel;
+    }
+
+    public void setWheel(Wheel wheel) {
+        this.wheel = wheel;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((engine == null) ? 0 : engine.hashCode());
+        result = prime * result + ((manufacturer == null) ? 0 : manufacturer.hashCode());
+        result = prime * result + maxSpeed;
+        long temp;
+        temp = Double.doubleToLongBits(maxVolumeOfTank);
+        result = prime * result + (int) (temp ^ (temp >>> 32));
+        result = prime * result + ((model == null) ? 0 : model.hashCode());
+        result = prime * result + speed;
+        temp = Double.doubleToLongBits(volumeOfFuel);
+        result = prime * result + (int) (temp ^ (temp >>> 32));
+        result = prime * result + ((wheel == null) ? 0 : wheel.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Car other = (Car) obj;
+        if (engine == null) {
+            if (other.engine != null)
+                return false;
+        } else if (!engine.equals(other.engine))
+            return false;
+        if (manufacturer == null) {
+            if (other.manufacturer != null)
+                return false;
+        } else if (!manufacturer.equals(other.manufacturer))
+            return false;
+        if (maxSpeed != other.maxSpeed)
+            return false;
+        if (Double.doubleToLongBits(maxVolumeOfTank) != Double.doubleToLongBits(other.maxVolumeOfTank))
+            return false;
+        if (model == null) {
+            if (other.model != null)
+                return false;
+        } else if (!model.equals(other.model))
+            return false;
+        if (speed != other.speed)
+            return false;
+        if (Double.doubleToLongBits(volumeOfFuel) != Double.doubleToLongBits(other.volumeOfFuel))
+            return false;
+        if (wheel == null) {
+            if (other.wheel != null)
+                return false;
+        } else if (!wheel.equals(other.wheel))
+            return false;
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return manufacturer + " " + model + "\nMax volume of tank: " + maxVolumeOfTank + "\nVolume of fuel: "
+                + volumeOfFuel + "\nMax speed: " + maxSpeed + "\nSpeed: " + speed + "\n#Engine\n" + engine
+                + "\n#Wheel\n" + wheel + "\n";
+    }
+
 }
--- Task12/src/main/java/by/epam/classes/entity/Engine.java
+++ Task12/src/main/java/by/epam/classes/entity/Engine.java
@@ -1,4 +1,108 @@
 package by.epam.classes.entity;
 
 public class Engine {
+    private int numberOfCylinders;
+    private double capacity;
+    private int power;
+    private String fuelType;
+    private double fuelFor100km;
+
+    public Engine(int numberOfCylinders, double capacity, int power, String fuelType, double fuelFor100km) {
+        this.numberOfCylinders = numberOfCylinders;
+        this.capacity = capacity;
+        this.power = power;
+        this.fuelType = fuelType;
+        this.fuelFor100km = fuelFor100km;
+    }
+
+    public Engine() {
+
+    }
+
+    public int getNumberOfCylinders() {
+        return numberOfCylinders;
+    }
+
+    public void setNumberOfCylinders(int numberOfCylinders) {
+        this.numberOfCylinders = numberOfCylinders;
+    }
+
+    public double getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(double capacity) {
+        this.capacity = capacity;
+    }
+
+    public int getPower() {
+        return power;
+    }
+
+    public void setPower(int power) {
+        this.power = power;
+    }
+
+    public String getFuelType() {
+        return fuelType;
+    }
+
+    public void setFuelType(String fuelType) {
+        this.fuelType = fuelType;
+    }
+
+    public double getFuelFor100rm() {
+        return fuelFor100km;
+    }
+
+    public void setFuelFor100rm(double fuelFor100rm) {
+        this.fuelFor100km = fuelFor100rm;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        long temp;
+        temp = Double.doubleToLongBits(capacity);
+        result = prime * result + (int) (temp ^ (temp >>> 32));
+        temp = Double.doubleToLongBits(fuelFor100km);
+        result = prime * result + (int) (temp ^ (temp >>> 32));
+        result = prime * result + ((fuelType == null) ? 0 : fuelType.hashCode());
+        result = prime * result + numberOfCylinders;
+        result = prime * result + power;
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Engine other = (Engine) obj;
+        if (Double.doubleToLongBits(capacity) != Double.doubleToLongBits(other.capacity))
+            return false;
+        if (Double.doubleToLongBits(fuelFor100km) != Double.doubleToLongBits(other.fuelFor100km))
+            return false;
+        if (fuelType == null) {
+            if (other.fuelType != null)
+                return false;
+        } else if (!fuelType.equals(other.fuelType))
+            return false;
+        if (numberOfCylinders != other.numberOfCylinders)
+            return false;
+        if (power != other.power)
+            return false;
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Number of cylinders: " + numberOfCylinders + "\nCapacity: " + capacity + "\nPower: " + power + "\nFuel type: "
+                + fuelType + "\nNeed fuel for 100 km: " + fuelFor100km;
+    }
+
 }
