c3c8108a44e7cb498835eb1039c523c0fffbcf76
Dmitry Gurinovich <dmitry.gurinovich@hotmail.com>
2021-03-19 13:17:07 +0300

    Task complete

--- Task14/src/main/java/by/epam/classes/main/Main.java
+++ Task14/src/main/java/by/epam/classes/main/Main.java
@@ -7,6 +7,6 @@ package by.epam.classes.main;
  */
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello Dmitry!");
     }
 }
